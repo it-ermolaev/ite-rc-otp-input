c2899da29a57ebf69eadd2aeedfa408243d09389
it-ermolaev <it.ermolaev@gmail.com>
2024-06-24 13:51:53 +0300

    Configured .lintstaged.rc.js apps/demo, packages/lint-staged, packages/library

--- apps/demo/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-const base = require("@repo/lint-staged");
-
-module.exports = {
-  ...base,
-  "{src}/**/*.{ts, tsx}": [() => "npm run check-types", "npm run lint:fix"],
-};
--- packages/library/.lintstagedrc.cjs
+++ packages/library/.lintstagedrc.cjs
@@ -1,5 +1,7 @@
 const baseConfig = require("@repo/lint-staged/base");
+const clientConfig = require("@repo/lint-staged/client");
 
 module.exports = {
   ...baseConfig,
+  ...clientConfig,
 };
